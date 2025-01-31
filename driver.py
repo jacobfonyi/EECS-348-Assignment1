61c68a522ff5b2d3c922cca07e16ec9df7b43bd7
jacobfonyi <157507311+jacobfonyi@users.noreply.github.com>
2025-01-30 21:20:07 -0600

    Update driver.py

--- driver.py
+++ driver.py
@@ -5,24 +5,30 @@
 
 
 
-from hospital import Hospital
+from maxheap import MaxHeap
+from email import Email
 
 def main():
     filename = input("Enter file: ")
-    hospital = Hospital()
+    inbox = Inbox()
     with open(filename, 'r') as file:
         for line in file:
             command = line.strip().split(maxsplit=1)
             if command[0] == "EMAIL":
                 _, details = command
-                sender_category, subject_line, date = details.split(maxsplit=3)
-                hospital.arrive(first_name, last_name, int(age), illness, int(severity))
+                sender_category, subject_line, date = details.split(",", 2)
+                maxheap.push(Email(sender_category, subject_line, date))
             elif command[0] == "NEXT":
-                inbox.next_email()
+                next_email = maxheap.peek()
+                if next_email:
+                    print("Next email:")
+                    print(next_email)
+                else:
+                    print("No emails to read.")
             elif command[0] == "READ":
-                inbox.read()
+                maxheap.pop()
             elif command[0] == "COUNT":
-                inbox.count()
+                print(f"There are {self.maxheap.count()} email(s) to read.")
                 
 if __name__ == "__main__":
     main()
